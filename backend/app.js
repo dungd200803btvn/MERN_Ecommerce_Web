c1b16f0671dc199335aa932496183412d3340fb5
Lê Chí Dũng <dung.lc@sis.hust.edu.vn>
2023-09-14 00:17:47 +0700

    Making Order API

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,13 @@ const errorMiddleWare = require("./middleware/error");
 const cookieParser = require("cookie-parser");
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoutes");
+const order = require("./routes/orderRoute");
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1",product);
 app.use("/api/v1",user);
+app.use("/api/v1", order);
 app.use(errorMiddleWare);
 
 
